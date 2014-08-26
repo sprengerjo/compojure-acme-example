c8fe56f8d773a291f1080ab356040a58910b57df
Jonas Sprenger <jonas.sprenger@gmx.de>
2014-08-26 22:44:48 +0200

    refactored tests to use midje

--- project.clj
+++ project.clj
@@ -4,9 +4,11 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.1.8"]
                  [ring/ring-core "1.2.0"]
-                 [ring/ring-json "0.2.0"]]
+                 [ring/ring-json "0.2.0"]
+                 [midje "1.6.3"]]
   :plugins [[lein-ring "0.8.11"]]
   :ring {:handler acme.handler/app}
   :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+  {:dev {:plugins [[lein-midje "3.1.1"]]
+        :dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring-mock "0.1.5"]]}})
--- test/acme/test/handler.clj
+++ test/acme/test/handler.clj
@@ -1,14 +1,12 @@
 (ns acme.test.handler
   (:require [clojure.test :refer :all]
             [acme.handler :refer :all]
-            [ring.mock.request :as mock]))
+            [ring.mock.request :as mock])
+   (:use midje.sweet))
 
-(deftest test-app
-  (testing "main route"
-    (let [response (app (mock/request :get "/"))]
-      (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
-  
-  (testing "not-found route"
-    (let [response (app (mock/request :get "/invalid"))]
-      (is (= (:status response) 404)))))
+(facts 
+  "main route"
+  (app (mock/request :get "/")) =>  (contains [[:status 200], [:body  "Hello World"]])
+  "not-found route"
+  (app (mock/request :get "/invalid")) => (contains [[:status 404]])
+)
