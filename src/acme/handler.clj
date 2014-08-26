8c03666a1f9c62389c41066968816f12bb8fe878
Benedikt Ritter <beneritter@gmail.com>
2014-08-26 21:00:12 +0200

    Inital commit

---
+++ src/acme/handler.clj
@@ -0,0 +1,34 @@
+(ns acme.handler
+  (:require [compojure.core :refer :all]
+            [compojure.handler :as handler]
+            [compojure.route :as route]
+            [ring.util.response :refer [resource-response response]]
+            [ring.middleware.json :as middleware]))
+
+(def data
+  {:1 { :name "Bene"
+        :email "bene@example.com" },
+   :2 { :name "Jonas"
+        :email "jonas@example.com" },
+   :3 { :name "Stephan"
+        :email "stephan@example.com" }})
+
+(defn get-data [key]
+  "Gets the value for key from data"
+  (data (keyword key)))
+
+(defroutes app-routes
+  (GET "/" [] "Hello World")
+  (GET "/data/" [] (response data))
+  (GET "/data/:id" [id] (response (get-data id)))
+  (GET "/data/:id/:field" [id field] (response ((get-data id) (keyword field))))
+  (route/resources "/")
+  (route/not-found "Not Found"))
+
+(def app
+;  (-> (handler/site app-routes)
+;      (middleware/wrap-json-body)
+;      (middleware/wrap-json-response)))
+(middleware/wrap-json-response
+  (middleware/wrap-json-body
+    (handler/site app-routes))))
